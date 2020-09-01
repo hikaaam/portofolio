09a718dab2527ae06121f11d4520f8479417d394
ilyas <ilyasyusuf018@gmail.com>
2020-09-02 02:03:27 +0700

    02-09-2020

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// Back to top
+var amountScrolled = 200;
+var amountScrolledNav = 25;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled ) {
+    $('button.back-to-top').addClass('show');
+  } else {
+    $('button.back-to-top').removeClass('show');
+  }
+});
+
+
+
